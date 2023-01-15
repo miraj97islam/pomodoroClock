a25fb86950293b7eb1fa5a179f9684b4706c64f1
MD MIRAJ ISLAM <52193756+miraj97islam@users.noreply.github.com>
2023-01-15 11:34:41 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
-let plusButton = document.getElementById('plusButton');
+let plusButtonOfDuration = document.getElementById('plusButtonOfDuration');
 let pomodoroDuration = document.getElementById('pomodoroDuration');
-let minusButton = document.getElementById('minusButton');
+let minusButtonOfDuration = document.getElementById('minusButtonOfDuration');
 
+let plusButtonOfBreak = document.getElementById('plusButtonOfBreak');
+let breakTimeDuration = document.getElementById('breakTimeDuration');
+let minusButtonOfBreak = document.getElementById('minusButtonOfBreak');
 
+let minuteDiv = document.getElementById('minute');
+let secondDiv = document.getElementById('second');
 
-plusButton.addEventListener('click', plus)
+minuteDiv.innerHTML = pomodoroDuration.innerHTML;
 
-function plus(){
-  if(parseInt(pomodoroDuration.innerHTML) < 25){
-    if(parseInt(pomodoroDuration.innerHTML) > 0 && parseInt(pomodoroDuration.innerHTML) < 9){
-      pomodoroDuration.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) + 1}`
-    }
-    else{
-      pomodoroDuration.innerHTML = parseInt(pomodoroDuration.innerHTML) + 1 ;
-    };
-  
-  }
-};
+plusButtonOfDuration.addEventListener('click', plusOfDuration);
+minusButtonOfDuration.addEventListener('click', minusOfDuration);
 
-minusButton.addEventListener('click', minus);
+plusButtonOfBreak.addEventListener('click', plusOfBreak);
+minusButtonOfBreak.addEventListener('click', minusOfBreak);
 
-function minus(){
-
-if(parseInt(pomodoroDuration.innerHTML) > 1){
-      if(parseInt(pomodoroDuration.innerHTML) < 11){
-        pomodoroDuration.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) - 1}`
+function plusOfDuration(){
+    if(parseInt(pomodoroDuration.innerHTML) < 25){
+      if(parseInt(pomodoroDuration.innerHTML) > 0 && parseInt(pomodoroDuration.innerHTML) < 9){
+        minuteDiv.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) + 1}`;
+        pomodoroDuration.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) + 1}`;
+       
       }
       else{
-        pomodoroDuration.innerHTML = parseInt(pomodoroDuration.innerHTML) - 1 ;
+        minuteDiv.innerHTML = parseInt(pomodoroDuration.innerHTML) + 1;
+        pomodoroDuration.innerHTML = parseInt(pomodoroDuration.innerHTML) + 1 ;
+        
       };
-}
+    
+    }
 };
+
+
+function minusOfDuration(){
+    if(parseInt(pomodoroDuration.innerHTML) > 1){
+          if(parseInt(pomodoroDuration.innerHTML) < 11){
+            minuteDiv.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) - 1}`
+            pomodoroDuration.innerHTML = `0${parseInt(pomodoroDuration.innerHTML) - 1}`
+          }
+          else{
+            minuteDiv.innerHTML = parseInt(pomodoroDuration.innerHTML) - 1 ;
+            pomodoroDuration.innerHTML = parseInt(pomodoroDuration.innerHTML) - 1 ;
+          };
+    }
+  };
+
+function plusOfBreak(){
+    if(parseInt(breakTimeDuration.innerHTML) < 10){
+      if(parseInt(breakTimeDuration.innerHTML) > 0 && parseInt(breakTimeDuration.innerHTML) < 9){
+        breakTimeDuration.innerHTML = `0${parseInt(breakTimeDuration.innerHTML) + 1}`
+      }
+      else{
+        breakTimeDuration.innerHTML = parseInt(breakTimeDuration.innerHTML) + 1 ;
+      };
+    
+    }
+}
+
+
+function minusOfBreak(){
+    if(parseInt(breakTimeDuration.innerHTML) > 1){
+      if(parseInt(breakTimeDuration.innerHTML) < 11){
+        breakTimeDuration.innerHTML = `0${parseInt(breakTimeDuration.innerHTML) - 1}`
+      }
+      else{
+        breakTimeDuration.innerHTML = parseInt(breakTimeDuration.innerHTML) - 1 ;
+      };
+  }
+}
